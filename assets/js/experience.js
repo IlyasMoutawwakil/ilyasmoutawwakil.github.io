997d8a05f50886142ed97734d712ccb299da5f2f
ilyas.moutawwakil <ilyas.moutawwakil@sysnav.fr>
2021-12-21 00:55:24 +0100

    first commit

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -2,39 +2,48 @@ AOS.init();
 
 //  Work experience cards
 
-
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Data Science Intern",
+    cardImage: "assets/images/sysnav.png",
+    place: "SYSNAV",
+    time: "(July, 2021 - Present)",
+    desp: `
+    As part of the <b>Traffic Security</b> team, my missions were as follows:
+    <ul>
+    <li><b>Retrieval, Processing and Visualization</b> of data from the <b>ELK Stack</b>.</li>
+    <li><b>Exploratory Data Analysis</b> of hundreds of millions of <b>Spatio-Temporal Data</b> points.</li>
+    <li>Study of <b>Statistical Forcasting</b> algorithms (parameterized and non parameterized).</li>
+    <li><b>Fine-Tuning</b> these algorithms for performance metrics Forecasting.</li>
+    <li><b>Deployment</b> of a platform (DashBoard) for <b>Monitoring, Visualisation and Forecasting</b>.</li>
+    </ul>
+    `,
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
-  },
-  {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Website Administrator",
+    cardImage: "https://automatants.cs-campus.fr/images/logo_blanc.png",
+    place: "Automatants - Association d'IA de CentraleSupélec",
+    time: "(January, 2020 - January, 2021)",
+    desp: `
+    On the campus of the <b>University of Paris Saclay</b>, I was responsible for:
+    <ul>
+    <li><b>Maintaining & Developping</b> the association's website.</li>
+    <li>Co-organising the association's annual event <b>Soirée de l'IA</b>.</li>
+    <li>Organising a <b>Lecture & Tutorial</b> about <b>CNN architectures</b>.</li>
+    <li>Organising a <b>Workshop</b> about the use of the <b>Google Colab</b>.</li>
+    </ul>
+    `,
   },
 ];
 
-const showCards2 = () => {
+const showCards1 = () => {
   let output = "";
   exp.forEach(
     ({ title, cardImage, place, time, desp }) =>
       (output += `        
     <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
-      <div class="card card1">
+      <div class="card card1" style="height:350px">
         <img src="${cardImage}" class="featured-image"/>
         <article class="card-body">
           <header>
@@ -56,40 +65,23 @@ const showCards2 = () => {
   );
   experiencecards.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards2);
-
+document.addEventListener("DOMContentLoaded", showCards1);
 
 // Volunteership Cards
 
 const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
-    title: "GirlScript Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/1.jpg",
-    description:
-      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
-  },
-  {
-    title: "StudentCode-in 2020",
-    cardImage: "assets/images/experience-page/2.jpg",
-    description:
-      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
-  },
-  {
-    title: "PClub Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/3.jpg",
-    description:
-      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
-  },
-  {
-    title: "Hakin-Codes",
-    cardImage: "assets/images/experience-page/4.jpg",
-    description:
-      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+    title: "sktime",
+    cardImage:
+      "https://raw.githubusercontent.com/alan-turing-institute/sktime/main/docs/source/images/sktime-logo-no-text.jpg",
+    description: `A unified framework for Time Series Analysis and Machine Learning.
+      <br>Contribution: Adding features, enhancing models (Prophet) and fixing bugs along the way.
+      .`,
   },
 ];
 
-const showCards = () => {
+const showCards2 = () => {
   let output = "";
   volunteershipcards.forEach(
     ({ title, cardImage, description }) =>
@@ -106,112 +98,24 @@ const showCards = () => {
   );
   volunteership.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards);
-
+document.addEventListener("DOMContentLoaded", showCards2);
 
 // Hackathon Section
-
 
 const hackathonsection = document.querySelector(".hackathon-section");
 const mentor = [
   {
-    title: "The Uplift Project",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/uplift.png",
-    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
+    title: "Huawei European University Challenge 2020",
+    subtitle: "Finalist",
+    image: "assets/images/huawei.png",
+    desp: "I participated alongside a other great members of the Automatants Association to this DataThon where we learned a lot about optical modeling and used state of the art Deep Learning architectures to perform this task.",
   },
-  {
-    title: "ULHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/ulhacks.png",
-    desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
-  },
-  {
-    title: "WaffleHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/wafflehacks.png",
-    desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
-  },
-  {
-    title: "Elevate Tech",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/elevate.png",
-    desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
-  },
-  {
-    title: "PitchTeen",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/pitchteen.png",
-    desp: "PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.",
-  },
-  {
-    title: "Hack-A-Solution",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/hackasolution.png",
-    desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
-  },
-  {
-    title: "UniGlobe Hacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/uniglobe.png",
-    desp: "UniGlobe Hacks 2.0 is an upcoming 4-day long hackathon run by high school students held during 28-31 July 2021.",
-  },
-  {
-    title: "AtlasHacks",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/atlas.png",
-    desp: "AtlasHacks II is a 3-day virtual hackathon that aims to foster creativity within hackers all around the world.",
-  },
-  {
-    title: "NeoHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/neo.png",
-    desp: "NeoHacks is an upcoming 48-hour hackathon taking place July 23-25, 2021 where hundreds of students from around the world will come together virtually to learn new skills, meet new friends, and build awesome projects. We provide beginner-friendly workshops, mentorship, fun games, and more.",
-  },
-  {
-    title: "Mission Inspired",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/mission.png",
-    desp: "InspireEd Hacks is a nationwide initiative organized by the student-for-student nonprofit, Mission InspirEd, challenging students ages 10-18 to synthesize their creativity and coding experience into a product beneficial to the educational community.",
-  },
-  {
-    title: "Hack3",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/hack3.png",
-    desp: "Hack3 2021 is an upcoming global online hackathon that aims to enable high school students to learn computer science, with a broader message of (everyone can code) targeting underrepresented demographics to improve diversity and inclusion of individuals in the computer technology space.",
-  },
-  {
-    title: "JITHack",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/jithack.png",
-    desp: "JITHack is an upcoming virtual design and development hackathon organized by the students of Jyothy Institute of Technology, Bengaluru on June 26, 2021, where participants have 24 hours to build something incredible.",
-  },
-  {
-    title: "Recess Hacks",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/recess.png",
-    desp: "Recess Hacks is an upcoming hackathon for high school students in the Greater Toronto Area that strives to encourage innovation among high school students and get involved in technology.",
-  },
-  {
-    title: "Citro Tech",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/citro.png",
-    desp: "Citro Hacks 2021 is an upcoming all-inclusive online hackathon organized by Citro Tech, a student led organization dedicated to empowering minorities in the tech field</li>",
-  },
-  {
-    title: "NHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/nhacks.png",
-    desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
-  }
-  
-
 ];
 
 const showCards3 = () => {
   let output = "";
   mentor.forEach(
-    ({ title, image, subtitle, desp}) =>
+    ({ title, image, subtitle, desp }) =>
       (output += `  
       <div class="blog-slider__item swiper-slide">
         <div class="blog-slider__img">
