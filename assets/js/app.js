54ae7fa0d3b6fb724de7ea0d2d1362c9709155e7
ilyas.moutawwakil <ilyas.moutawwakil@sysnav.fr>
2021-12-21 20:45:14 +0100

    added form functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -130,11 +130,10 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
-                <form>
-                  <form action="https://formcarry.com/s/S2thQbCqEvW" method="POST" accept-charset="UTF-8" >
-                  <input type="text" name="field1" placeholder="Your Name" required/>
-                  <input type="email" name="field2" placeholder="Email Address"  required/>
-                  <textarea name="field3" placeholder="Type your Message" required></textarea>
+                <form action="https://formcarry.com/s/xyVDQlrqL1E" method="POST" accept-charset="UTF-8" >
+                  <input type="text" name="name" placeholder="Your Name" required/>
+                  <input type="email" name="email" placeholder="Email Address"  required/>
+                  <textarea name="message" placeholder="Type your Message" required></textarea>
                   <input type="submit" value="Send" />
                 </form>
              </div>
